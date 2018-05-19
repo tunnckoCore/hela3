3ed602214d615eb1e30043b95306ee49baed97d0
Charlike Mike Reagent <olsten.larck@gmail.com>
2018-05-19 07:55:27 +0300

    refactor
    Signed-off-by: Charlike Mike Reagent <olsten.larck@gmail.com>

---
+++ packages/cli/src/cli.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+const esmLoader = require('esm');
+
+const main = esmLoader(module)('./index.mjs');
+
+main()
+  .then(() => {
+    // This is a CLI file, so please!
+    // eslint-disable-next-line unicorn/no-process-exit
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.error('Failed command:', err.commandName);
+
+    if (err.commandArgv && err.commandArgv['show-stack']) {
+      console.error('Error stack:', err.stack);
+    } else {
+      console.error(`${err.name}:`, err.message);
+    }
+
+    // This is a CLI file, so please!
+    // eslint-disable-next-line unicorn/no-process-exit
+    process.exit(1);
+  });
---
+++ packages/cli/src/index.mjs
@@ -0,0 +1,38 @@
+import { hela } from '@hela/core';
+
+const cli = hela();
+
+/**
+ * Hela's CLI options and commands
+ */
+
+cli.commandless(() => cli.help());
+
+cli.option('--show-stack', 'Show error stack trace when command fail', false);
+
+/**
+ * TODO: loading of tasks/presets/config files
+ *
+ * @returns {Promise}
+ */
+async function main() {
+  const configModule = await import('./hela.config');
+  const preset = Object.assign({}, configModule);
+
+  if (preset.default && typeof preset.default === 'function') {
+    const meta = preset.default.getMeta();
+    const taskName = meta.usage.split(' ')[0];
+    preset[taskName] = preset.default;
+    delete preset.default;
+  }
+
+  const tasks = Object.keys(preset).reduce((acc, name) => {
+    acc[name] = preset[name].getMeta();
+    return acc;
+  }, {});
+
+  cli.tree = Object.assign({}, cli.tree, tasks);
+  return cli.listen();
+}
+
+export default main;
