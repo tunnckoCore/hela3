2efea7a631d7c98bdfe7a5e1072c16c9d1f0a896
Charlike Mike Reagent <olsten.larck@gmail.com>
2018-06-09 01:58:27 +0300

    chore: update all packages to use xaxa
    Signed-off-by: Charlike Mike Reagent <olsten.larck@gmail.com>

--- packages/config-tunnckocore/src/index.mjs
+++ packages/config-tunnckocore/src/index.mjs
@@ -1,12 +1,11 @@
-/* eslint-disable import/prefer-default-export */
 import { hela } from '@hela/core';
 
 const cli = hela();
 
 export const lint = cli
   .command('lint')
-  .describe('Lint all `.mjs` files from `src/` and `test/` directories')
-  .action('eslint src test -f codeframe --ext .mjs --quiet --fix --cache');
+  .describe('Lint all files from `src/` and `test/` directories')
+  .action('xaxa');
 
 export const test = cli
   .command('test')
