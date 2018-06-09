61c9102b3bb3bb031ce04f9f9ed4983a64c2cf6c
Charlike Mike Reagent <olsten.larck@gmail.com>
2018-06-09 04:32:34 +0300

    chore: force updates
    Signed-off-by: Charlike Mike Reagent <olsten.larck@gmail.com>

--- packages/config-tunnckocore/src/index.mjs
+++ packages/config-tunnckocore/src/index.mjs
@@ -1,11 +1,12 @@
-import { hela } from '@hela/core';
+import { hela, shell } from '@hela/core';
+import dargs from '@hela/dargs';
 
 const cli = hela();
 
 export const lint = cli
   .command('lint')
   .describe('Lint all files from `src/` and `test/` directories')
-  .action('xaxa');
+  .action((argv) => shell(`xaxa ${dargs(argv).join(' ')}`));
 
 export const test = cli
   .command('test')
