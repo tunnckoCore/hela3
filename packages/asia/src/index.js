0a3f5ebf6e887e6b56375353c637cadbe1026b0e
Charlike Mike Reagent <olsten.larck@gmail.com>
2018-06-09 04:08:50 +0300

    chore: pull `asia` out of monorepo
    Signed-off-by: Charlike Mike Reagent <olsten.larck@gmail.com>

--- packages/asia/src/index.js
+++
@@ -1,6 +0,0 @@
-const esmLoader = require('esm');
-
-const esmRequire = esmLoader(module);
-const m = esmRequire('./index.mjs');
-
-module.exports = typeof m === 'object' && 'default' in m ? m.default : m;
--- packages/asia/test/index.mjs
+++
@@ -1,7 +0,0 @@
-import test from '../src';
-
-test('asia', (t) => {
-  console.log('ooooooookey');
-
-  t.pass(111);
-});
