9cc008b81dfc68688409ee58cbd92ff1339b88f0
Charlike Mike Reagent <opensource@tunnckocore.com>
2019-06-18 20:43:44 +0300

    chore: add linting with jest
    Signed-off-by: Charlike Mike Reagent <opensource@tunnckocore.com>

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,16 @@ const createBuildConfig = require('./build.config');
 
 module.exports = {
   projects: [
+    {
+      displayName: 'lint',
+
+      testEnvironment: 'node',
+      testMatch: ['<rootDir>/packages/**/*'],
+      testPathIgnorePatterns: ['.+\\.d\\.ts$', '.+/dist/.+'],
+
+      runner: 'jest-runner-eslint',
+      moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'mjs'],
+    },
     createBuildConfig({ NODE_ENV: 'main' }),
     createBuildConfig({ NODE_ENV: 'module' }),
   ],
