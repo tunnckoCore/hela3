7f1412591298502e785d34cb5b3f77e78c48d284
Charlike Mike Reagent <opensource@tunnckocore.com>
2019-06-19 15:03:58 +0300

    feat(@hela/dev): add example lint command
    Signed-off-by: Charlike Mike Reagent <opensource@tunnckocore.com>

--- packages/hela-dev/src/index.js
+++ packages/hela-dev/src/index.js
@@ -10,9 +10,15 @@ import createBuildConfig from './configs/build';
 import {} from './support';
 
 const prog = hela();
+const helaDevRoot = path.dirname(__dirname);
+const helaDevNodeBins = path.join(helaDevRoot, 'node_modules', '.bin', 'jest');
 
-function createJestConfig(...args) {
-  const projects = [].concat(args).filter(Boolean);
+function createJestConfig(proj, opts) {
+  const projects = []
+    .concat(proj)
+    .map((cfg) => cfg(opts))
+    .filter(Boolean);
+
   const hash = toHash(projects);
 
   const content = `module.exports={projects:${JSON.stringify(projects)}};`;
@@ -30,50 +36,68 @@ function exists(fp, opts) {
   return fs.existsSync(path.join(opts.cwd, fp));
 }
 
+async function genericAction(argv) {
+  const opts = Object.assign({ cwd: proc.cwd(), type: 'build' }, argv, {
+    env: { NODE_ENV: 'main' },
+  });
+
+  opts.mono = exists('packages', opts) || exists('lerna.json', opts);
+
+  const cfgMainPath = path.join(__dirname, 'configs', opts.type, '.config');
+  const cfgCwdPathHash = toHash(opts.cwd);
+  const cfgPath = `${cfgMainPath}-${cfgCwdPathHash}.js`;
+
+  const { hash, content } = createJestConfig(opts.projects, opts);
+
+  try {
+    const { hash: OldHash } = createJestConfig((await import(cfgPath)).default);
+
+    if (hash !== OldHash) {
+      fs.unlinkSync(cfgPath);
+      throw new Error('fake one, to the catch');
+    }
+  } catch (err) {
+    await util.promisify(fs.writeFile)(cfgPath, content, 'utf-8');
+  }
+
+  return exec([
+    `node ${helaDevNodeBins} --version`,
+    `node ${helaDevNodeBins} --config ${cfgPath}`,
+  ]);
+}
 /**
  * Build command, uses Babel through Jest, to compile the code.
  */
 export const build = prog
   .command('build', 'Build project with Babel using Jest')
-  .action(async (argv) => {
-    const opts = Object.assign({ cwd: proc.cwd() }, argv, {
-      env: { NODE_ENV: 'main' },
-    });
+  .action((argv) =>
+    genericAction({
+      ...argv,
+      projects: [
+        (opts) => createBuildConfig(opts),
+        (opts) => createBuildConfig({ ...opts, env: { NODE_ENV: 'module' } }),
+      ],
+    }),
+  );
 
-    opts.mono = exists('packages', opts) || exists('lerna.json', opts);
+export const foo = prog.command('foo').action((argv) =>
+  genericAction({
+    ...argv,
+    projects: [
+      (opts) => ({
+        displayName: 'lint',
 
-    const helaDevRoot = path.dirname(__dirname);
-    const helaNodeBins = path.join(helaDevRoot, 'node_modules', '.bin', 'jest');
+        testEnvironment: 'node',
+        testMatch: ['<rootDir>/packages/**/*'],
+        testPathIgnorePatterns: ['.+\\.d\\.ts$', '.+/dist/.+'],
 
-    const cfgMainPath = path.join(__dirname, 'configs', 'build', '.config');
-    const cfgCwdPathHash = toHash(opts.cwd);
-    const cfgPath = `${cfgMainPath}-${cfgCwdPathHash}.js`;
-
-    const { hash, content } = createJestConfig(
-      createBuildConfig(opts),
-      createBuildConfig({ ...opts, env: { NODE_ENV: 'module' } }),
-    );
-
-    try {
-      const { hash: OldHash } = createJestConfig(
-        (await import(cfgPath)).default,
-      );
-
-      if (hash !== OldHash) {
-        fs.unlinkSync(cfgPath);
-        throw new Error('fake one, to the catch');
-      }
-    } catch (err) {
-      await util.promisify(fs.writeFile)(cfgPath, content, 'utf-8');
-    }
-
-    return exec([
-      `node ${helaNodeBins} --version`,
-      `node ${helaNodeBins} --config ${cfgPath}`,
-    ]);
-  });
-
-export const foo = prog.command('foo').action(() => {});
+        runner: 'jest-runner-eslint',
+        moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'mjs'],
+        rootDir: opts.cwd,
+      }),
+    ],
+  }),
+);
 // export const test = 'NODE_ENV=test jest';
 // export const lint = 'jest --config jest-lint.config.js';
 // export const build2 = 'jest --config jest-build.config.js';
